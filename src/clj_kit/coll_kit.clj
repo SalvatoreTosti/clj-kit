0c6ad997fcb8d6cd4e4a21ea4f7c286d1b5b5621
Salvatore Tosti <saltosti@gmail.com>
2017-07-17 18:12:11 -0700

    Added docstrings for coll-kit functions

--- src/clj_kit/coll_kit.clj
+++ src/clj_kit/coll_kit.clj
@@ -1,6 +1,7 @@
 (ns clj-kit.coll-kit)
 
 (defn coll-contains?
+  "Returns true if element is present in given collection, acts like 'contains?' for list and vector."
   [coll item]
   (cond
    (list? coll) (not (neg? (.indexOf coll item)))
@@ -9,6 +10,7 @@
   (contains? coll item)))
 
 (defn remove-nth
+  "Removes nth element from a collection, returns a map with keys :target and :new-coll."
   [coll index]
   (cond
    (nil? coll) nil
@@ -24,10 +26,12 @@
      {:target target :new-coll (concat head tail)})))
 
 (defn remove-random
+  "Removes a random element from a given collection."
   [coll]
   (remove-nth coll (rand-int (count coll))))
 
-(defn select-random-rec
+(defn- select-random-rec
+  "Helper function used to recursively select a random number of items from a given collection."
   [coll selected number]
   (if (or (not (pos? number))
           (empty? coll))
@@ -36,10 +40,12 @@
       (select-random-rec (:new-coll removal) (conj selected (:target removal)) (dec number)))))
 
 (defn select-random
+  "Selects a random number of items from a given collection."
   [coll number]
   (select-random-rec coll '() number))
 
 (defn split-nth
+  "Splits collections at the nth element, returns a map with keys :target, :head, and :tail."
   [coll index]
   (cond
    (nil? coll) nil
@@ -53,25 +59,16 @@
      {:target target :head head :tail tail})))
 
 (defn remove-random
-  [vect]
-  (remove-nth vect (rand-int (count vect))))
-
-(defn- select-random-rec
-  [coll selected number]
-  (if (or (not (pos? number))
-          (empty? coll))
-    selected
-  (let [removal (remove-random coll)]
-    (select-random-rec (:new-coll removal) (conj selected (:target removal)) (dec number)))))
-
-(defn select-random
-  [coll number]
-  (select-random-rec coll '() number))
+  "Removes a random item from a given list, returns a map with keys :target and :new-coll."
+  [coll]
+  (remove-nth coll (rand-int (count coll))))
 
 (defn rand-nth-keys
+  "Returns a random key from a given map."
   [coll]
   (rand-nth (keys coll)))
 
 (defn rand-nth-values
+  "Returns a random value from a given map."
   [coll]
   (coll (rand-nth-keys coll)))
