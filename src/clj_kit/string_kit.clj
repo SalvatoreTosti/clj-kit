9d0250546e54d4012bf35ac4161ef54573743c00
Salvatore Tosti <saltosti@gmail.com>
2017-07-25 19:55:49 -0700

    Update string-kit docs

--- src/clj_kit/string_kit.clj
+++ src/clj_kit/string_kit.clj
@@ -1,6 +1,7 @@
 (ns clj-kit.string-kit)
 
 (defn is-only-alpha?
+  "Determines if a string only contains letters."
   [st]
   (cond
    (nil? st) false
@@ -10,6 +11,7 @@
         (every? true?))))
 
 (defn contains-alpha?
+  "Determines if a string contains at least 1 letter."
   [st]
   (cond
    (nil? st) false
@@ -21,6 +23,7 @@
         (not))))
 
 (defn is-lower?
+  "Determines if all characters in a string are lowercase."
   [st]
   (cond
    (nil? st) false
@@ -30,6 +33,7 @@
         (every? true?))))
 
 (defn is-upper?
+  "Determines if all characters in a string are uppercase."
   [st]
   (cond
    (nil? st) false
@@ -39,12 +43,14 @@
         (every? true?))))
 
 (defn is-integer?
+  "Determines if a given string is an integer."
   [st]
   (try
     (when (Integer/parseInt st) true)
     (catch Exception e false)))
 
 (defn is-double?
+  "Determines if a given string is a double."
   [st]
   (try
     (when (Double/parseDouble st) true)
